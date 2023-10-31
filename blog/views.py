ed81ef4ce4d3a1343cafc8b430790528741e4704
Alain Serrano <alains1984@gmail.com>
2023-10-31 18:08:40 +0100

    Updated views and forms

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import(render, get_object_or_404)
+from django.shortcuts import(render, get_object_or_404, redirect)
 from django.core.paginator import (Paginator, EmptyPage,\
     PageNotAnInteger)
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -53,3 +54,17 @@ def post_share(request, post_id):
     
     return render(request, 'blog/post/share.html', {'form':form, 'post':post,'sent':sent})
         
+    
+@require_POST    
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request,'blog/post/comment.html',
+                  {'post':post,
+                   'form':form,
+                   'comment':comment})
