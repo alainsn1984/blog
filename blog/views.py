e348df91c96255ba2984aaedbad6289cd4759503
Alain Serrano <alains1984@gmail.com>
2023-11-02 08:57:00 +0100

    Added comment and comment_form pages

--- blog/views.py
+++ blog/views.py
@@ -33,7 +33,9 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request,'blog/post/detail.html', {'post':post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request,'blog/post/detail.html', {'post':post,'comments':comments,'form':form})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post,id=post_id, status=Post.Status.PUBLISHED)
@@ -68,3 +70,4 @@ def post_comment(request, post_id):
                   {'post':post,
                    'form':form,
                    'comment':comment})
+    
